70467a2a0e3916a4718e391740bf893c0bfb7d61
x4u3n <h1y0s1lv3r@gmail.com>
2017-01-30 17:26:25 +0000

    cambios en mpdstat

--- mpdstat_pidgin.sh
+++ mpdstat_pidgin.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 while : ; do
      
-	mpc idle
+	mpc idle &> /dev/null
 	NOTIFY=$(mpc current)
-	notify-send -i /usr/share/icons/gnome/16x16/actions/player_record.png "$NOTIFY"
-    nohup purple-remote "setstatus?message=Reproduciendo >> `mpc current` << https://github.com/AlmostLook/hiyosilver"
+	notify-send -i /usr/share/icons/gnome/16x16/actions/player_record.png ">>" "$NOTIFY" 
+    purple-remote "setstatus?message=Playing now >> `mpc --format  "%artist% - %title%\nALBUM:%album% DATE:%date% GENRE:%genre%" | head -n2` << https://github.com/AlmostLook/hiyosilver" &
 done
 
