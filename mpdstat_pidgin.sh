a71e200bbff715e7eb1334e1cee9a97b08fe01ec
x4u3n <h1y0s1lv3r@gmail.com>
2017-01-30 19:05:01 +0000

    Estado de pidgin - mpc current & libnotify

--- mpdstat_pidgin.sh
+++ mpdstat_pidgin.sh
@@ -3,7 +3,7 @@ while : ; do
      
 	mpc idle &> /dev/null
 	NOTIFY=$(mpc current)
-	notify-send -i /usr/share/icons/gnome/16x16/actions/player_record.png ">>" "$NOTIFY" 
-    purple-remote "setstatus?message=Playing now >> `mpc --format  "%artist% - %title%\nALBUM:%album% DATE:%date% GENRE:%genre%" | head -n2` << https://github.com/AlmostLook/hiyosilver" &
+	notify-send -i "audio-headphones" " " "$NOTIFY" 
+    purple-remote "setstatus?message=Playing now >> `mpc --format  "[[%artist% -  ]%title%]\nALBUM: %album% DATE: %date% GENRE: %genre%" | head -n2` << https://github.com/AlmostLook/hiyosilver" &
 done
 
