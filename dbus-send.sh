fc8c986e9c4901bf0a170576cadabb14a1f41e7a
AlmostLook <juanluis.csb@gmail.com>
2017-05-07 13:31:19 +0200

    Delete dbus-send.sh

--- dbus-send.sh
+++
@@ -1,7 +0,0 @@
-dbus-send --session --dest=org.freedesktop.Notifications \
-                         --type=method_call --reply-timeout=10000 \
-                         /org/freedesktop/Notifications org.freedesktop.Notifications \
-                         string:'Test Application' uint32:0 \
-                         string: string:'Notification Title' \
-                         string:'This is the body' \
-                         array:string: dict:string: int32:10000
