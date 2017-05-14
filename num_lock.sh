0d61b225ea1da70c1b7448803b44f2e30d2ae429
x4u3n <h1y0s1lv3r@gmail.com>
2017-05-14 19:39:35 +0200

    :)

--- num_lock.sh
+++
@@ -1,2 +0,0 @@
-xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape' &
-numlockx &
--- xdotool_f5_chromium.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Refresh the current frame in chromium
-xdotool search --name ".*- chromium$" key ctrl+r
